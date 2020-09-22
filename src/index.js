5202cc151d05888eec3a853f007afc421f841a3d
Iva Kochneva <inkybasik@gmail.com>
2020-09-22 22:54:52 +0300

    Чтоб собиралось

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './css/style.css';
-import './img/goblin.png';
 import './js/app';
 
 // TODO: write your code in app.js
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 'production';
 
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common');
